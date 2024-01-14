bf0a580249192924ae3dd9f7f1786b07c84f5053
abdallahawad3 <djdgdnnxcbj@gmail.com>
2024-01-14 13:14:25 +0200

    ⚡

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,29 @@ let showMenu = document.querySelector(".menu");
 let backToTop = document.querySelector(".back-to-top");
 let navListItem = document.querySelectorAll(".nav-list li");
 
+let nums = document.querySelectorAll(".stats-content .num");
+let section = document.querySelector(".stats");
+let started = false;
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
 backToTop.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
